656990502e144dcb3bd85463d30ce067df67a3d7
fazna023 <fasnafasy22@gmail.com>
2024-11-28 17:36:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    const menuIcon = document.getElementById("menu-icon");
+    const navList = document.getElementById("navlist");
+
+    menuIcon.addEventListener("click", () => {
+        navList.classList.toggle("active");
+    });
